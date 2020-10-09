363e87cd87e9013b679edf0aba4e5caf9e14d429
opilane <opilane@HP-CZC6297BB1>
2020-10-09 12:37:02 +0300

    Add project files.

---
+++ Tunnplaan1/Tunnplaan1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tunnplaan1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
